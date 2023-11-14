60e9d596a42bb07624ab92a120b81fc9525dd304
Nikolas Mello <nikolasmarcos@gmail.com>
2023-11-14 01:03:20 -0300

    forms

--- src/app/pages/cadastro/pessoa/morador/morador.component.ts
+++ src/app/pages/cadastro/pessoa/morador/morador.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { IconDefinition, faFloppyDisk, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition, faFloppyDisk, faXmark, faPenToSquare, faTrash, faEraser, faSearch, faAdd } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { iPerson } from 'src/app/pages/interfaces/Iperson';
+import { dataPersons } from 'src/app/pages/mock/mock-data';
+import Pagination from 'src/app/util/pagination';
 
 @Component({
   selector: 'app-morador',
@@ -13,11 +16,22 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 })
 export default class MoradorComponent implements OnInit {
 
+  faEraser: IconDefinition = faEraser;
+  faSearch: IconDefinition = faSearch;
+  faAdd: IconDefinition = faAdd;
+  faTrash: IconDefinition = faTrash;
+  faPenToSquare: IconDefinition = faPenToSquare;
   faFloppyDisk: IconDefinition = faFloppyDisk;
   faXmark: IconDefinition = faXmark;
   formCadastroMorador: FormGroup;
+  formFiltro: FormGroup;
+
   update: boolean = false;
-  acao: boolean = true;
+  create: boolean = false;
+  filtro: boolean = true;
+
+  paginationPerson: Pagination<iPerson> = new Pagination<iPerson>(5, dataPersons);
+  personList:iPerson[] = dataPersons;
 
   constructor(
     private fb: FormBuilder
@@ -27,9 +41,12 @@ export default class MoradorComponent implements OnInit {
 
   ngOnInit(): void {
     this.createFormCadastroMorador();
+    this.createFormFiltro();
+    this.paginationPerson.getPage(1);
+    console.log('pages', this.paginationPerson)
   }
 
-  createFormCadastroMorador(){
+  createFormCadastroMorador(person?: iPerson){
     this.formCadastroMorador = this.fb.group({
       idMorador: '',
       nome: ['' , Validators.required],
@@ -44,8 +61,51 @@ export default class MoradorComponent implements OnInit {
     });
   }
 
+  createFormFiltro(){
+    this.formFiltro = this.fb.group({
+      nome: ['' ],
+      endereco: [''],
+      email: ['']
+    })
+  }
+
   salvar(){
 
+  }
+
+  pesquisar(){
+
+  }
+
+  cadastrar(){
+    this.create = true;
+    this.update = false;
+    this.filtro = false;
+  }
+
+  editar(person: iPerson){
+    this.update = true;
+    this.filtro = false;
+    this.create = false;
+    this.createFormCadastroMorador(person);
+  }
+
+  limpar(){
+    this.formFiltro.reset();
+  }
+
+  fecharCadastro(){
+    this.create = false;
+    this.update = false;
+    this.filtro = true;
+    this.formCadastroMorador.reset();
+  }
+
+  getPage(pageNumber: number | string){
+    if( typeof pageNumber === 'number'){
+      this.paginationPerson.getPage(pageNumber);
+    }
+    
   }
 
 }
